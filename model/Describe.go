6025ef6ce99fc105bfac018cb4e250911f7c61ff
halosky-Wendy <34014848+halosky-Wendy@users.noreply.github.com>
2022-11-06 16:14:11 +0800

    updated Describe model

--- model/Describe.go
+++ model/Describe.go
@@ -6,6 +6,23 @@ type BaseDescribe struct {
 	sver     string `json:"sver" gorm:"column:sver"`
 	location string `json:"location" gorm:"column:location"`
 }
+type LineDescribe struct {
+	consumes    string `json:"consumes" gorm:"column:consumes"`
+	operationId string `json:"operation_id" gorm:"column:operation_id"`
+	paramsId    string `json:"params_id" gorm:"column:params_id"`
+	produces    string `json:"produces" gorm:"column:produces"`
+	summary     string `json:"summary" gorm:"column:summary"`
+	tags        string `json:"tags" gorm:"column:tags"`
+}
 
+type Param struct {
+	description string `json:"description" gorm:"column:description"`
+	format      string `json:"format" gorm:"column:format"`
+	in          string `json:"in" gorm:"column:in"`
+	name        string `json:"name" gorm:"column:name"`
+	required    string `json:"required" gorm:"column:required"`
+	types       string `json:"types" gorm:"column:type"`
+}
 
-type 
+type LineOfParam struct {
+}
