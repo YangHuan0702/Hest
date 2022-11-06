856341579a05b55aea43004e0d474a80b27d454c
halosky-Wendy <34014848+halosky-Wendy@users.noreply.github.com>
2022-11-06 16:15:37 +0800

    updated field authority

--- model/Describe.go
+++ model/Describe.go
@@ -1,27 +1,27 @@
 package model
 
 type BaseDescribe struct {
-	name     string `json:"name" gorm:"column:name"`
-	url      string `json:"url" gorm:"column:url"`
-	sver     string `json:"sver" gorm:"column:sver"`
-	location string `json:"location" gorm:"column:location"`
+	Name     string `json:"name" gorm:"column:name"`
+	Url      string `json:"url" gorm:"column:url"`
+	Sver     string `json:"sver" gorm:"column:sver"`
+	Location string `json:"location" gorm:"column:location"`
 }
 type LineDescribe struct {
-	consumes    string `json:"consumes" gorm:"column:consumes"`
-	operationId string `json:"operation_id" gorm:"column:operation_id"`
-	paramsId    string `json:"params_id" gorm:"column:params_id"`
-	produces    string `json:"produces" gorm:"column:produces"`
-	summary     string `json:"summary" gorm:"column:summary"`
-	tags        string `json:"tags" gorm:"column:tags"`
+	Consumes    string `json:"consumes" gorm:"column:consumes"`
+	OperationId string `json:"operation_id" gorm:"column:operation_id"`
+	ParamsId    string `json:"params_id" gorm:"column:params_id"`
+	Produces    string `json:"produces" gorm:"column:produces"`
+	Summary     string `json:"summary" gorm:"column:summary"`
+	Tags        string `json:"tags" gorm:"column:tags"`
 }
 
 type Param struct {
-	description string `json:"description" gorm:"column:description"`
-	format      string `json:"format" gorm:"column:format"`
-	in          string `json:"in" gorm:"column:in"`
-	name        string `json:"name" gorm:"column:name"`
-	required    string `json:"required" gorm:"column:required"`
-	types       string `json:"types" gorm:"column:type"`
+	Description string `json:"description" gorm:"column:description"`
+	Format      string `json:"format" gorm:"column:format"`
+	In          string `json:"in" gorm:"column:in"`
+	Name        string `json:"name" gorm:"column:name"`
+	Required    string `json:"required" gorm:"column:required"`
+	Types       string `json:"types" gorm:"column:type"`
 }
 
 type LineOfParam struct {
